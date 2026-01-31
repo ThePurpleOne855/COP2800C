cfa4fe951629fc3e94fbb5fe6c76407d2e34c691
Jose Armando Santana <jasas1357@gmail.com>
2026-01-31 09:54:36 -0500

    Update RecursiveMethodAssignment.java

--- COP2800C/src/RecursiveMethodAssignment.java
+++ COP2800C/src/RecursiveMethodAssignment.java
@@ -1,11 +1,15 @@
-
+/*Recursive Method Assignment*/
 public class RecursiveMethodAssignment {
 	public static void main(String[] args) {
+		// Create Variables
+		
 		int numberOfCycles = 10;
 		int counter = 1;
+
+		// Call the printRecursive method
 		printRecursive(counter, numberOfCycles);
 	}
-	
+	// Defining the printing method
 	static void printRecursive(int counter, int cycles) {
 		if (cycles == 0) {
 			return;
