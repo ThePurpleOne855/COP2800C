231caca8bf5803bc1b1cd774270b949fa1e58929
Jose Armando Santana <jasas1357@gmail.com>
2026-02-07 13:38:42 -0500

    Update Methods_IO_ExceptionsAssignment.java

--- COP2800C/src/Methods_IO_ExceptionsAssignment.java
+++ COP2800C/src/Methods_IO_ExceptionsAssignment.java
@@ -1,44 +1,28 @@
-import java.util.Scanner;
+import java.util.Scanner;  // Imports
 import java.util.InputMismatchException;
 
 public class Methods_IO_ExceptionsAssignment {
+	// Initialize Scanner
 	Scanner input = new Scanner(System.in);
 	
 	public static void main(String[] args) {
+		// Initialize instances of classes.
 		Methods_IO_ExceptionsAssignment program = new Methods_IO_ExceptionsAssignment();
 		DoesNotmakeAnySenseButForAssignment program2 = new DoesNotmakeAnySenseButForAssignment();
-		String userName = program.name();
+		String userName = program.name(); // Calling the methods defined in each class.
 		int UserAge = program2.age();
 		program.output(userName, UserAge);
 		
-		program.input.close();
+		program.input.close(); // Closing both Scanner instances.
+		program2.input.close();
 	}
 	
 	public String name() {
 		System.out.println("What's your government name?\n");
 		String name = input.nextLine();
 		return name;
-	}
-	
-	public int age() {
-		System.out.println("How old are you?\n");
-		while(true) {
-			try {
-				int age = input.nextInt();
-				if(age < 0) {
-					System.out.println("Your age cannot be less than 0.");
-					throw new InputMismatchException();
-				}
-				input.nextLine();
-				return age;
-				
-			} catch(InputMismatchException e) {
-				System.out.println("That's not a valid input.");
-				input.nextLine();
-			}
-		}
-		
-	}
+	}		
+}
 	
 	public void output(String userName, int userAge) {
 		
@@ -60,14 +44,15 @@ class DoesNotmakeAnySenseButForAssignment {
 		while(true) {
 			try {
 				int age = input.nextInt();
-				if(age < 0) {
+				if(age < 0) { // Will take advantage of the catch already defined even though it should not work for this check
+							  // Will throw the exception manually so it asks the user for a new input.
 					System.out.println("Your age cannot be less than 0.");
 					throw new InputMismatchException();
 				}
 				input.nextLine();
 				return age;
 				
-			} catch(InputMismatchException e) {
+			} catch(InputMismatchException e) { // if the user inputs something that is not an integer.
 				System.out.println("That's not a valid input.");
 				input.nextLine();
 			}
